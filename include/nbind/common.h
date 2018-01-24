e1f5351896ab2d5a36f7bcea7c36483e01519afe
Arnaud Botella <arnaud.botella@total.com>
2018-01-24 18:26:23 +0100

    Update common.h

--- include/nbind/common.h
+++ include/nbind/common.h
@@ -7,10 +7,10 @@
 
 namespace nbind {
 
-std::forward_list<BindClassBase *> & nbind_api getClassList(void);
-std::forward_list<MethodDef> & nbind_api getFunctionList(void);
-const void ** nbind_api getPrimitiveList(void);
-const void ** nbind_api getNamedTypeList(void);
+std::forward_list<BindClassBase *>& nbind_api getClassList(void);
+std::forward_list<MethodDef>& nbind_api getFunctionList(void);
+const void** nbind_api getPrimitiveList(void);
+const void** nbind_api getNamedTypeList(void);
 
 void nbind_api registerClass(BindClassBase &bindClass);
 void nbind_api registerFunction(
